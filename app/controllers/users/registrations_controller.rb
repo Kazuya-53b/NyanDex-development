a54bcc3b4c1cc8b8c9d54ac9675d8726eddc591d
rq-Kazuya-53b-at-321899771753 <kazukichi18@outlook.jp>
2024-09-26 11:16:20 +0900

    Add user login and login errors

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -43,7 +43,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # アカウント更新時のパラメータを許可する
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
   end
 
   # サインアップ後のリダイレクト先
