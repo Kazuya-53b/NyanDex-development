d4f9b3b72ca0f7e271230c495275400817bc57fb
rq-Kazuya-53b-at-321899771753 <kazukichi18@outlook.jp>
2024-09-26 09:52:43 +0900

    Add devise controllers

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  def new
+    super
+  end
+
+  # POST /resource
+  def create
+    super
+  end
+
+  # GET /resource/edit
+  def edit
+    super
+  end
+
+  # PUT /resource
+  def update
+    super
+  end
+
+  # DELETE /resource
+  def destroy
+    super
+  end
+
+  # GET /resource/cancel
+  def cancel
+    super
+  end
+
+  protected
+
+  # 追加のパラメータを許可する場合は、ここに追加します。
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+  end
+
+  # アカウント更新時のパラメータを許可する
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password])
+  end
+
+  # サインアップ後のリダイレクト先
+  def after_sign_up_path_for(resource)
+    super(resource)
+  end
+
+  # 非アクティブなアカウントのサインアップ後のリダイレクト先
+  def after_inactive_sign_up_path_for(resource)
+    super(resource)
+  end
+end
