7455d195a6115b74bdc9e44b0ce2d49bef8b1fad
rq-Kazuya-53b-at-321899771753 <kazukichi18@outlook.jp>
2024-09-28 20:52:18 +0900

    Edit flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,15 @@ class UsersController < ApplicationController
 
   def update_username
     @user = User.find(params[:id])
-
+  
     if @user.update(user_params)
+      flash.now[:notice] = "ユーザー名が更新されました。"  # 通常のフラッシュを使用
       respond_to do |format|
         format.turbo_stream do
-          render turbo_stream: turbo_stream.replace("user-info-container", partial: "users/edit_username", locals: { user: @user })
+          render turbo_stream: [
+            turbo_stream.replace("user-info-container", partial: "users/edit_username", locals: { user: @user }),
+            turbo_stream.replace("flash-message-container", partial: "layouts/flash_message")  # フラッシュメッセージ更新
+          ]
         end
         format.html { redirect_to @user, notice: "ユーザー名が更新されました。" }
       end
@@ -27,7 +31,7 @@ class UsersController < ApplicationController
         format.html { render :edit }
       end
     end
-  end
+  end    
 
   private
 
