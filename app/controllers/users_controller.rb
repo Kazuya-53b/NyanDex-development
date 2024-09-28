58c9d5c9f146717fd690ad1d35aab2ea413173c2
rq-Kazuya-53b-at-321899771753 <kazukichi18@outlook.jp>
2024-09-28 15:12:57 +0900

    Fix lint errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,13 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    
+
     if @user.update(user_params)
       respond_to do |format|
         format.turbo_stream do
           render turbo_stream: turbo_stream.replace("user-info-container", partial: "users/edit_username", locals: { user: @user })
         end
-        format.html { redirect_to @user, notice: 'ユーザー名が更新されました。' }
+        format.html { redirect_to @user, notice: "ユーザー名が更新されました。" }
       end
     else
       respond_to do |format|
@@ -27,12 +27,11 @@ class UsersController < ApplicationController
         format.html { render :edit }
       end
     end
-  end    
-  
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:username)
   end
 end
-
