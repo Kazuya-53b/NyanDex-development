0279ef95fec9cfa4ca7d90f5c294d7a3e3955403
rq-Kazuya-53b-at-321899771753 <kazukichi18@outlook.jp>
2024-09-28 15:05:53 +0900

    Add user information page

--- db/schema.rb
+++ db/schema.rb
@@ -30,11 +30,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_27_014836) do
     t.string "username"
     t.string "email"
     t.string "profile_image"
-    t.string "crypted_password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "encrypted_password"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "encrypted_password"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "cats", "users"
