6bf4cf2ca4939a5f01e5a4cac15db6dce58c17b6
rq-Kazuya-53b-at-321899771753 <kazukichi18@outlook.jp>
2024-10-01 18:24:42 +0900

    Add relationship between User and Cat models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :cats
+
   validates :username, presence: true, length: { maximum: 12 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
