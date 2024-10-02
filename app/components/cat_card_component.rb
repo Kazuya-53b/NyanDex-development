62c71b171b17a391094ad8ef51650e978d7072d5
rq-Kazuya-53b-at-321899771753 <kazukichi18@outlook.jp>
2024-10-02 22:22:03 +0900

    Create profile cards

--- app/components/cat_card_component.rb
+++ app/components/cat_card_component.rb
@@ -1,11 +1,17 @@
 class CatCardComponent < ViewComponent::Base
+  with_collection_parameter :cat
+
   def initialize(cat:)
     @cat = cat
   end
 
   def call
-    content_tag :div, class: "border p-4 rounded-lg w-44 h-36" do
+    content_tag :div, class: "card-container" do
+      image = @cat.cat_images.first&.image_url || asset_path('sample_cat_image.jpeg')
       safe_join([
+        content_tag(:div, class: "image-container") do
+          image_tag(image, class: "square-image")
+        end,
         content_tag(:h3, @cat.name, class: "font-bold"),
         content_tag(:p, "#{@cat.age} 歳", class: "text-gray-500"),
         content_tag(:p, @cat.gender, class: "text-gray-500"),
--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,6 +1,6 @@
 class CatsController < ApplicationController
   def index
-    @cats = Cat.includes(:user)
+    @cats = Cat.includes(:user).limit(20)
     respond_to do |format|
       format.html
       format.turbo_stream
